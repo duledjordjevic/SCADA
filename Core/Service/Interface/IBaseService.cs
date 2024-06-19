d029cd3f01ca21f8ccfe88e0dbf3e0420050c654
Rade Pejanović <radepejanovic@gmail.com>
2024-06-19 12:53:48 +0200

    fix: Fixed .sln issue

---
+++ Core/Service/Interface/IBaseService.cs
@@ -0,0 +1,17 @@
+﻿using Core.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Service
+{
+    public interface IBaseService
+    {
+        void InitNotifier();
+
+        void SendMessage(string message);
+    }
+}
---
+++ Core/Service/Interface/ITagService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace Core.Service
+{
+    [ServiceContract(CallbackContract = typeof(ICallBack))]
+    public interface ITagService
+    {
+        [OperationContract]
+        void SetOutput(double value);
+
+        [OperationContract]
+ 
+        void GetOutput(double value);
+        [OperationContract]
+        
+        void ToggleScan();
+        [OperationContract]
+        
+        void AddTag();
+        [OperationContract]
+        
+        void RemoveTag(double value);
+    }
+}
