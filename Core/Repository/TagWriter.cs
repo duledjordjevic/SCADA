b92cc2654ef14f6c61392305e5cac1d24c2cc43e
Dusan Djordjevic <djordjevic.sv1.2021@uns.ac.rs>
2024-06-19 18:55:24 +0200

    feat: add Reader and Writer for Tags from xml

---
+++ Core/Repository/TagWriter.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Core.Model;
+
+namespace Core.Repository
+{
+    public class TagWriter
+    {
+        public static List<XElement> GetXmlAnalogInputs(List<AnalogInput> analogInputs)
+        {
+            return analogInputs.Select(tag => new XElement("AI",
+                new XAttribute("name", tag.Name),
+                new XAttribute("address", tag.Address),
+                new XAttribute("syncTime", tag.SyncTime),
+                new XAttribute("isSyncTurned", tag.IsSyncTurned.ToString().ToLower()),
+                //new XAttribute("driver", tag.Driver),
+                new XAttribute("lowLimit", tag.LowLimit),
+                new XAttribute("highLimit", tag.HighLimit),
+                new XAttribute("unit", tag.Unit),
+                new XElement("description", tag.Description),
+                new XElement("alarms",
+                    tag.Alarms.Select(alarm =>
+                        new XElement("alarm",
+                            new XAttribute("priorityType", alarm.PriorityType.ToString()),
+                            new XAttribute("priority", alarm.Priority),
+                            new XAttribute("threshold", alarm.Threshold)
+                        )
+                    )
+                )
+            )).ToList();
+        }
+
+        public static List<XElement> GetXmlAnalogOutputs(List<AnalogOutput> analogOutputs)
+        {
+            return analogOutputs.Select(output => new XElement("AO",
+                new XAttribute("name", output.Name),
+                new XAttribute("address", output.Address),
+                new XAttribute("value", output.Value),
+                new XAttribute("lowLimit", output.LowLimit),
+                new XAttribute("highLimit", output.HighLimit),
+                new XAttribute("unit", output.Unit),
+                new XElement("description", output.Description)
+            )).ToList();
+        }
+
+        public static List<XElement> GetXmlDigitalInputs(List<DigitalInput> digitalInputs)
+        {
+            return digitalInputs.Select(input => new XElement("DI",
+                new XAttribute("name", input.Name),
+                new XAttribute("address", input.Address),
+                new XAttribute("syncTime", input.SyncTime),
+                new XAttribute("isSyncTurned", input.IsSyncTurned.ToString().ToLower()),
+                new XAttribute("driver", input.Driver),
+                new XElement("description", input.Description)
+            )).ToList();
+        }
+
+        public static List<XElement> GetXmlDigitalOutputs(List<DigitalOutput> digitalOutputs)
+        {
+            return digitalOutputs.Select(output => new XElement("DO",
+                new XAttribute("name", output.Name),
+                new XAttribute("address", output.Address),
+                new XAttribute("value", output.Value),
+                new XElement("description", output.Description)
+            )).ToList();
+        }
+    }
+}
---
+++ Core/TagProccessing.cs
@@ -0,0 +1,61 @@
+﻿using Core.Model;
+using Core.Model.Enum;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Core.Repository;
+using System.IO;
+
+namespace Core
+{
+    
+    public class TagProccessing
+    {
+        static readonly string TAGS_CONFIG_PATH = @"";
+
+        static readonly object tagsConfigLock = new object();
+
+        static List<AnalogInput> analogInputs = new List<AnalogInput>();
+        static List<AnalogOutput> analogOutputs = new List<AnalogOutput>();
+        static List<DigitalInput> digitalInputs = new List<DigitalInput>();
+        static List<DigitalOutput> digitalOutputs = new List<DigitalOutput>();
+
+        
+
+        public static void LoadTags()
+        {
+            XElement xmlData = XElement.Load(TAGS_CONFIG_PATH);
+            analogInputs = TagReader.LoadAnalogInputs(xmlData.Descendants("AI"));
+            analogOutputs = TagReader.LoadAnalogOutputs(xmlData.Descendants("AO"));
+            digitalInputs = TagReader.LoadDigitalInputs(xmlData.Descendants("DI"));
+            digitalOutputs = TagReader.LoadDigitalOutputs(xmlData.Descendants("DO"));
+        }
+
+        public static void SaveTagConfiguration()
+        {
+            var analogInputsXml = TagWriter.GetXmlAnalogInputs(analogInputs);
+            var analogOutputsXml = TagWriter.GetXmlAnalogOutputs(analogOutputs);
+            var digitalInputsXml = TagWriter.GetXmlDigitalInputs(digitalInputs);
+            var digitalOutputsXml = TagWriter.GetXmlDigitalOutputs(digitalOutputs);
+
+            var tagsXml = new XElement("tags");
+            tagsXml.Add(analogInputsXml);
+            tagsXml.Add(analogOutputsXml);
+            tagsXml.Add(digitalInputsXml);
+            tagsXml.Add(digitalOutputsXml);
+
+            lock(tagsConfigLock)
+            {
+                using (var writer = new StreamWriter(TAGS_CONFIG_PATH))
+                {
+                    writer.Write(tagsXml);
+                }
+            }
+        }
+
+
+    }
+}
