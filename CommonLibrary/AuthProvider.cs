f35efba413de370a7216532b25b9b14cfce7552a
Rade Pejanović <radepejanovic@gmail.com>
2024-06-19 18:54:18 +0200

    feat: Implemented AuthProvider

---
+++ CommonLibrary/AuthProvider.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonLibrary
+{
+    public class AuthProvider
+    {
+        public static string GenerateSalt()
+        {
+            var crypto = new RNGCryptoServiceProvider();
+            byte[] salt = new byte[32];
+            crypto.GetBytes(salt);
+            return Convert.ToBase64String(salt);
+        }
+
+        public static string Encrypt(string password)
+        {
+            string salt = GenerateSalt();
+            byte[] saltedPassword = Encoding.UTF8.GetBytes(salt + password);
+            using (var sha = new SHA256Managed())
+            {
+                byte[] hash = sha.ComputeHash(saltedPassword);
+                return $"{Convert.ToBase64String(hash)}:{salt}";
+            }
+        }
+
+        public static bool Validate(string password, string encryptedPassword)
+        {
+            string[] arrValues = encryptedPassword.Split(':');
+            string passwordHash = arrValues[0];
+            string salt = arrValues[1];
+            byte[] saltedPassword = Encoding.UTF8.GetBytes(salt + password);
+            using (var sha = new SHA256Managed())
+            {
+                byte[] hash = sha.ComputeHash(saltedPassword);
+                string enteredValueToValidate = Convert.ToBase64String(hash);
+                return passwordHash.Equals(enteredValueToValidate);
+            }
+        }
+
+        public static string GenerateToken(string username)
+        {
+            RNGCryptoServiceProvider crypto = new RNGCryptoServiceProvider();
+            byte[] randVal = new byte[32];
+            crypto.GetBytes(randVal);
+            string randStr = Convert.ToBase64String(randVal);
+            return username + randStr;
+        }
+
+    }
+}
