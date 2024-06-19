d89841853d4bc88fa96acc8844f7c5daa009013b
Rade Pejanović <radepejanovic@gmail.com>
2024-06-19 11:11:17 +0200

    feat: Initialized base solution structure

---
+++ DatabaseManager/Program.cs
@@ -0,0 +1,32 @@
+﻿using DatabaseManager.ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ServiceModel;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatabaseManager
+{
+    public class Program
+    {
+        public class Callback : IUserServiceCallback
+        {
+            public void MessageArrived(string message)
+            {
+                Console.WriteLine(message);
+            }
+        }
+
+        static UserServiceClient userClient;
+
+        static void Main(string[] args)
+        {
+            InstanceContext ic = new InstanceContext(new Callback());
+            userClient = new UserServiceClient(ic);
+
+            userClient.Register("rade", "srade");
+            Console.ReadKey();
+        }
+    }
+}
