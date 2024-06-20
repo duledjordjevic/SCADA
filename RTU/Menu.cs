d36156d6ace1fad64bcb18096ae0f5db90b709ab
Rade Pejanović <radepejanovic@gmail.com>
2024-06-20 15:40:18 +0200

    feat: Created RTU simulation

---
+++ RTU/Menu.cs
@@ -0,0 +1,62 @@
+﻿using CommonLibrary.ConsoleTools;
+using RTU.ServiceReference;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace RTU
+{
+    public class Menu
+    {
+        private readonly RTDClientAdapter RTDClientAdapter;
+
+        private readonly string startMenu = @"
+                +-------- MENU --------+
+                |1) Register RTU       |
+                |X) Exit               |
+                +----------------------+
+            ";
+
+        private readonly string rtuTitle = @"
+                Real Time Unit Readings
+            ";
+
+        public Menu(RealTimeDriverServiceClient rtdClient)
+        {
+            RTDClientAdapter = new RTDClientAdapter(rtdClient);
+        }
+
+        public void StartMenu()
+        {
+            var registered = false;
+            while (!registered)
+            {
+                PrettyConsole.WriteTitle(startMenu);
+                PrettyConsole.Write("Enter choice: ");
+                var input = Console.ReadLine().ToLower();
+                switch (input)
+                {
+                    case "1":
+                        RTDClientAdapter.Register();
+                        RTDClientAdapter.Configure();
+                        registered = true;
+                        break;
+
+                    case "x":
+                        PrettyConsole.WriteLine("Exited.");
+                        return;
+
+                    default:
+                        PrettyConsole.WriteLine("Error: Invalid choice.");
+                        Thread.Sleep(1000);
+                        break;
+                }
+            }
+            PrettyConsole.WriteTitle(startMenu);
+            RTDClientAdapter.SendData();
+        }
+    }
+}
