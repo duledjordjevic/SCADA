1acff4f4ba6e0da37c9fbcc959be163de2e605e4
Rade Pejanović <radepejanovic@gmail.com>
2024-06-22 20:51:11 +0200

    feat: Implemented Report LINQs
    Collaborator: @umuskinja

--- Core/Repository/TagRepository.cs
+++ Core/Repository/TagRepository.cs
@@ -1,6 +1,8 @@
 ﻿using CommonLibrary.Model;
 using Core.Model;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Core.Repository
 {
@@ -23,6 +25,66 @@ namespace Core.Repository
                     });
 
                     db.SaveChanges();
+                }
+            }
+        }
+
+        public static List<TagEntity> GetTagValuesByPeriod(DateTime startTime, DateTime endTime)
+        {
+            lock (dbLock)
+            {
+                using (var db = new DatabaseContext())
+                {
+                    return db.TagValues
+                             .Where(tv => tv.Timestamp >= startTime && tv.Timestamp <= endTime)
+                             .OrderBy(tv => tv.Timestamp)
+                             .ToList();
+                }
+            }
+        }
+
+        public static List<TagEntity> GetLastAITagValues()
+        {
+            lock (dbLock)
+            {
+                using (var db = new DatabaseContext())
+                {
+                    return db.TagValues
+                             .Where(tv => tv.Type == "AITag")
+                             .GroupBy(tv => tv.TagName)
+                             .Select(g => g.OrderByDescending(tv => tv.Timestamp).FirstOrDefault())
+                             .OrderBy(tv => tv.Timestamp)
+                             .ToList();
+                }
+            }
+        }
+
+        public static List<TagEntity> GetLastDITagValues()
+        {
+            lock (dbLock)
+            {
+                using (var db = new DatabaseContext())
+                {
+                    return db.TagValues
+                             .Where(tv => tv.Type == "DITag")
+                             .GroupBy(tv => tv.TagName)
+                             .Select(g => g.OrderByDescending(tv => tv.Timestamp).FirstOrDefault())
+                             .OrderBy(tv => tv.Timestamp)
+                             .ToList();
+                }
+            }
+        }
+
+        public static List<TagEntity> GetTagValuesById(int tagId)
+        {
+            lock (dbLock)
+            {
+                using (var db = new DatabaseContext())
+                {
+                    return db.TagValues
+                             .Where(tv => tv.Id == tagId)
+                             .OrderBy(tv => tv.Value)
+                             .ToList();
                 }
             }
         }
